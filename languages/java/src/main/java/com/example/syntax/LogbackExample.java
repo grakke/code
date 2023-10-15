e5329ace1ba7b7870d033793029e4681b889109e
grakke <liboming88@yahoo.com>
2023-10-15 20:23:07 +0800

    update

---
+++ languages/java/src/main/java/com/example/syntax/LogbackExample.java
@@ -0,0 +1,11 @@
+package com.example.syntax;
+
+public class LogbackExample {
+
+//    private static final Logger logger
+//            = LoggerFactory.getLogger(LogbackExample.class);
+//
+//    public static void main(String[] args) {
+//        logger.info("Example log from {}", LogbackExample.class.getSimpleName());
+//    }
+}
