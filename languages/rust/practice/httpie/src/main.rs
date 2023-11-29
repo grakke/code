b1fe0a09d141ade1e9fc7e4f3124426381986365
grakke <liboming88@yeah.net>
2023-11-29 16:53:03 +0800

    fix httpie

--- languages/rust/practice/httpie/src/main.rs
+++ languages/rust/practice/httpie/src/main.rs
@@ -1,9 +1,10 @@
-use anyhow::{anyhow, Result};
+extern crate clap;
+use anyhow::{anyhow, Ok, Result};
 use clap::Parser;
-use colored::Colorize;
-use mime::Mime;
 use reqwest::{header, Client, Response, Url};
 use std::{collections::HashMap, str::FromStr};
+use colored::Colorize;
+use mime::Mime;
 use syntect::{
     easy::HighlightLines,
     highlighting::{Style, ThemeSet},
@@ -11,17 +12,15 @@ use syntect::{
     util::{as_24_bit_terminal_escaped, LinesWithEndings},
 };
 
-// 定义 HTTPie 的 CLI 主入口，包含若干个子命令
-// 下面 /// 注释是文档，clap 会将其作为 CLI 的帮助
-
-/// A naive httpie implementation with Rust, can you imagine how easy it is?
+// 定义 HTTPie 的 CLI 主入口，包含多个命令
+//cargo run -- post httpbin.org/post a=1 b=2
 #[derive(Parser, Debug)]
-#[command(version = "1.0", author = "Grakke <liboming88@yahoo.com")]
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
 }
 
+/// 子命令分别对应不同的 HTTP 方法
 #[derive(Parser, Debug)]
 enum SubCommand {
     Get(Get),
@@ -30,26 +29,19 @@ enum SubCommand {
 
 #[derive(Parser, Debug)]
 struct Get {
-    /// HTTP 请求 URL
-    #[clap(parse(try_from_str = parse_url))]
+    #[arg(value_parser=parse_url)]
     url: String,
 }
 
 #[derive(Parser, Debug)]
 struct Post {
-    #[clap(parse(try_from_str = parse_url))]
+    #[arg(value_parser=parse_url)]
     url: String,
-    #[clap(parse(try_from_str = parse_kv_pair))]
+    #[arg(value_parser=parse_kv_pair)]
     body: Vec<KvPair>,
 }
 
-fn parse_url(s: &str) -> Result<String> {
-    let _url: Url = s.parse()?;
-
-    Ok(s.into())
-}
-
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 struct KvPair {
     k: String,
     v: String,
@@ -59,36 +51,74 @@ impl FromStr for KvPair {
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut split = s.split("=");
+        // 使用 = 进行 split，这会得到一个迭代器
+        let mut split = s.split('=');
         let err = || anyhow!(format!("Failed to parse {}", s));
         Ok(Self {
             // 从迭代器中取第一个结果作为 key，迭代器返回 Some(T)/None
-            // 将其转换成 Ok(T)/Err(E)，然后用 ? 处理错误
+            // 我们将其转换成 Ok(T)/Err(E)，然后用 ? 处理错误
             k: (split.next().ok_or_else(err)?).to_string(),
+            // 从迭代器中取第二个结果作为 value
             v: (split.next().ok_or_else(err)?).to_string(),
         })
     }
 }
 
-/// 为 KvPair 实现 FromStr，可以直接 s.parse() 得到 KvPair
 fn parse_kv_pair(s: &str) -> Result<KvPair> {
     s.parse()
 }
 
-async fn get(client: Client, args: &Get) -> Result<()> {
-    let response = client.get(&args.url).send().await?;
-    Ok(print_resp(response).await?)
+fn parse_url(s: &str) -> Result<String> {
+    // check url
+    let _url: Url = s.parse()?;
+
+    Ok(s.into())
 }
 
+/// 处理 get 子命令
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+/// 处理 post 子命令
 async fn post(client: Client, args: &Post) -> Result<()> {
     let mut body = HashMap::new();
     for pair in args.body.iter() {
         body.insert(&pair.k, &pair.v);
     }
-    let response = client.post(&args.url).json(&body).send().await?;
-    Ok(print_resp(response).await?)
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
 }
 
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+
+    println!();
+}
+
+/// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &str) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
+        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
+
+        // 其它 mime type，我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+
+/// 打印整个响应
 async fn print_resp(resp: Response) -> Result<()> {
     print_status(&resp);
     print_headers(&resp);
@@ -98,43 +128,21 @@ async fn print_resp(resp: Response) -> Result<()> {
     Ok(())
 }
 
-// 严格要求同步
-fn print_status(resp: &Response) {
-    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
-    println!("{}\n", status);
-}
-
-fn print_headers(resp: &Response) {
-    for (name, value) in resp.headers() {
-        println!("{}: {:?}", name.to_string().green(), value);
-    }
-
-    println!("\n");
-}
-
+/// 将服务器返回的 content-type 解析成 Mime 类型
 fn get_content_type(resp: &Response) -> Option<Mime> {
     resp.headers()
         .get(header::CONTENT_TYPE)
         .map(|v| v.to_str().unwrap().parse().unwrap())
 }
 
-fn print_body(m: Option<Mime>, body: &String) {
-    match m {
-        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
-        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
-
-        _ => println!("{}", body),
-    }
-}
-
+/// 程序的入口函数，因为在 http 请求时我们使用了异步处理，所以这里引入 tokio
 #[tokio::main]
 async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
     let mut headers = header::HeaderMap::new();
-
+    // 为我们的 http 客户端添加一些缺省的 HTTP 头
     headers.insert("X-POWERED-BY", "Rust".parse()?);
     headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
-
     let client = reqwest::Client::builder()
         .default_headers(headers)
         .build()?;
@@ -152,7 +160,6 @@ fn print_syntect(s: &str, ext: &str) {
     let ts = ThemeSet::load_defaults();
     let syntax = ps.find_syntax_by_extension(ext).unwrap();
     let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
-
     for line in LinesWithEndings::from(s) {
         let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
         let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
@@ -175,7 +182,6 @@ mod tests {
     #[test]
     fn parse_kv_pair_works() {
         assert!(parse_kv_pair("a").is_err());
-        // TODO binary operation `==` cannot be applied to type `KvPair`
         assert_eq!(
             parse_kv_pair("a=1").unwrap(),
             KvPair {
