9e7607ccb4b0b25e099531e85956605243325235
grakke <liboming88@yeah.net>
2024-03-16 11:05:07 +0800

    add micro services code

---
+++ advance/micro_services/course/lab01/implicit-server/src/test/java/io/spring2go/implicitserver/ImplicitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring2go.implicitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ImplicitServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
--- languages/springboot/readinglist/src/main/java/com/manning/readinglist/ReadingListApplication.java
+++ languages/springboot/readinglist/src/main/java/com/manning/readinglist/ReadingListApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReadingListApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ReadingListApplication.class, args);
-    }
+	void main(String[] args) {
+		SpringApplication.run(ReadingListApplication.class, args);
+	}
 }
