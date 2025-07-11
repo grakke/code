414c945f02ed42b613bca7095e0eb83b5464daac
grakke <liboming88@yeah.net>
2025-07-11 13:14:24 +0800

    update vue

--- client/vue/vue3_demo/src/App.vue
+++ client/vue/vue3_demo/src/App.vue
@@ -18,6 +18,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/todo">TODO</RouterLink>
       </nav>
     </div>
   </header>
