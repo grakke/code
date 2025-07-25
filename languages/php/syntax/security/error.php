0a1c10caa173307f0b746c2c3116eb8b8793706c
grakke <liboming88@yeah.net>
2025-07-25 16:15:48 +0800

    feat: stage all modified files

--- languages/php/syntax/security/error.php
+++ languages/php/syntax/security/error.php
@@ -1,42 +1,41 @@
 <?php
 
 ini_set('display_errors', 1);
-//error_reporting(E_ALL);
 error_reporting(E_ALL ^ E_WARNING);
 
 /**
  * 自定义错误处理器
  *
- * @param $errno    int 错误级别
- * @param $errstr   string 错误信息
- * @param $errfile  string 错误文件
- * @param $errline  int   错误行号
+ * @param int $errNo
+ * @param string $errStr
+ * @param string $errFile
+ * @param int $errLine
  */
-function myErrorHandler($errno, $errstr, $errfile, $errline)
+function myErrorHandler(int $errNo, string $errStr, string $errFile, int $errLine): void
 {
     // 该级别错误不报告的话退出
-    if (!(error_reporting() & $errno)) {
+    if (!(error_reporting() & $errNo)) {
         return;
     }
-    $logDir = __DIR__."/..";
+    $logDir = __DIR__ . "/..";
     if (!file_exists($logDir)) {
         mkdir($logDir);
     }
-    $logFile = $logDir.DIRECTORY_SEPARATOR.'err.log';
+    $logFile = $logDir . DIRECTORY_SEPARATOR . 'err.log';
 
-//    logger("%file% %level% %message%", ["level" => "warning", "message" => "This is a message", "file" => __FILE__]);
-    switch ($errno) {
+    logger("%file% %level% %message%", ["level" => "warning", "message" => $errLine + ":" + $errStr, "file" => $errFile]);
+    switch ($errNo) {
         case E_ERROR:
-            error_log("[致命错误]:[".date('Y-m-d H:i:s', time())."]_[$errno] $errstr", 3, $logFile);
+            error_log("[致命错误]:[" . date('Y-m-d H:i:s', time()) . "]_[$errNo] $errStr", 3, $logFile);
             break;
         case E_WARNING:
-            error_log("[警告]:[".date('Y-m-d H:i:s', time())."]_[$errno] $errstr", 3, $logFile);
+            error_log("[警告]:[" . date('Y-m-d H:i:s', time()) . "]_[$errNo] $errStr", 3, $logFile);
             break;
         case E_NOTICE:
-            error_log("[通知]:[".date('Y-m-d H:i:s', time())."]_[$errno] $errstr", 3, $logFile);
+            error_log("[通知]:[" . date('Y-m-d H:i:s', time()) . "]_[$errNo] $errStr", 3, $logFile);
             break;
         default:
-            echo "[未知错误类型]:[".date('Y-m-d H:i:s', time())."]_[$errno] $errstr\n";
+            echo "[未知错误类型]:[" . date('Y-m-d H:i:s', time()) . "]_[$errNo] $errStr\n";
             break;
     }
 }
@@ -53,4 +52,27 @@ try {
     $context = file_get_contents('https://bluebird89.com/error', 0., $ctx);
 } catch (Error $e) {
     var_dump($e);
+}
+
+set_error_handler('handle_normal_error', E_ALL | E_STRICT);
+function handle_normal_error($errno, $errstr, $Serrfile, $errline)
+{
+    switch ($errno) {
+        case E_USER_ERROR:
+            send_error_to_log_server($errstr);
+            break;
+        case E_USER_WARNING:
+            break;
+        default:
+    }
+    return true;
+}
+
+register_shutdown_function('handle_fatal_error');
+function handle_fatal_error(): void
+{
+    $error = error_get_last();
+    if (isset($error['type']) && E_ERROR == $error['type']) {
+        send_error_to_log_server($msg);
+    }
 }
