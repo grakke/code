ee382922c80852b47b9a7694c77337b1a88934ef
grakke <liboming88@yeah.net>
2026-07-28 10:43:38 +0800

    update:http

--- languages/go/web/server.go
+++ languages/go/web/server.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "Hello World!0")
 }
 
 type HelloHandlerStruct struct {
@@ -17,6 +21,13 @@ type HelloHandlerStruct struct {
 
 func (handler *HelloHandlerStruct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, handler.content)
+}
+
+type WelcomeHandlerStruct struct {
+}
+
+func (*WelcomeHandlerStruct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome")
 }
 
 type Middleware func(http.HandlerFunc) http.HandlerFunc
@@ -50,9 +61,44 @@ func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
 	return f
 }
 
-func main() {
-	http.HandleFunc("/", Chain(HelloHandler, Method("GET"), Logging()))
-	http.Handle("/hello", &HelloHandlerStruct{content: "Hello World!"})
+// func main() {
+// 	http.HandleFunc("/", Chain(HelloHandler, Method("GET"), Logging()))
+// 	http.Handle("/hello", &HelloHandlerStruct{content: "Hello World!"})
 
-	http.ListenAndServe(":8000", nil)
+//		http.ListenAndServe(":8000", nil)
+//	}
+//
+// 自定义 mux
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HelloHandler)
+	mux.Handle("/welcome", &WelcomeHandlerStruct{})
+	// http.ListenAndServe(":8080", mux)
+
+	// 优雅关停
+	server := &http.Server{
+		Addr:    ":8081",
+		Handler: mux,
+	}
+
+	// 创建系统信号接收器
+	done := make(chan os.Signal)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-done
+
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Fatal("Shutdown server:", err)
+		}
+	}()
+
+	log.Println("Starting HTTP server...")
+	err := server.ListenAndServe()
+	if err != nil {
+		if err == http.ErrServerClosed {
+			log.Print("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpected")
+		}
+	}
 }
