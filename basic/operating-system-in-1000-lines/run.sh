c27f40626e2e2069e36fc942688ef49ca1b60af6
grakke <liboming88@yeah.net>
2026-08-06 16:31:07 +0800

    add:printf

--- basic/operating-system-in-1000-lines/run.sh
+++ basic/operating-system-in-1000-lines/run.sh
@@ -10,7 +10,7 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld
 
 # 构建内核
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
+    kernel.c common.c
 
 # 启动 QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -monitor telnet::45454,server,nowait\
